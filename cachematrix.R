42ce352475e0499690617c01c6b8964410d76a97
mvilab <mvila@MacBook-Pro-de-Miquel.local>
2014-11-21 13:25:48 +0100

    Assignment completed
    Used the provided example as a base for completing the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment2 - Caching the inverse of a matrix
 
-## Write a short comment describing this function
-
+## [makeCacheMatrix] given a matrix, returns an "object" that keeps a cache of
+## the inverse of that matrix.
+## Avaliable methods:
+##  - get : return currently set matrix
+##  - set : sets a new matrix, deletes old cache
+##  - setinv : caches the given inverse
+##  - getinv : returns the currently cached inverse (or NULL)
 makeCacheMatrix <- function(x = matrix()) {
-
+  ix <- NULL
+  set <- function(y) {
+    x <<- y
+    ix <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) ix <<- inv
+  getinv <- function() ix
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
+## [cacheSolve] given a CacheMatrix, returns the inverse of that matrix without
+## recalculating if the matrix inverse has been previosly calculated.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Get cached inverse
+  ix <- x$getinv()
+  ## Return the cached inverse if avaliable
+  if(!is.null(ix)) {
+    message("getting cached data")
+    return(ix)
+  }
+  # No cached inverse! Get original matrix
+  data <- x$get()
+  # Calculate the inverse
+  ix <- solve(data, ...)
+  # Cache the inverse 
+  x$setinv(ix)
+  # Return inverse
+  ix
 }
